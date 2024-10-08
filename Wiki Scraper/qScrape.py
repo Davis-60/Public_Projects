bd9f29e55123a1aa0aa1b909a8934ecb874a8195
davisfeatherstone <featherstone.davis@gmail.com>
2024-10-08 10:19:22 +0100

    overall improvements

--- Wiki Scraper/qScrape.py
+++ Wiki Scraper/qScrape.py
@@ -1,57 +1,55 @@
+from collections import deque
 import requests
 from bs4 import BeautifulSoup
 
 #I will need a wikiPage object to store the info for each page I visit
 class wikiPage:
-    def __init__(self, url, linksAway):
+    def __init__(self, url, linksAway, path):
         self.url = url
         self.linksAway = linksAway
+        self.path = path
+
+#This function will create the final text once a match is found
+def generateReport(urlMatch: wikiPage, targetTerm:str, soup: BeautifulSoup):
+        report = "\nFound!\n" + "\nURL: "+ str(urlMatch.url) +"\nPath: " + " -> ".join(urlMatch.path) +"\nLinks Away: " + str(urlMatch.linksAway)
+        # Find all text elements in the current text that contain the targetTerm
+        text_elements = soup.find_all(string=lambda text: text and targetTerm in text)
+        # Finding the line containing the match for the final report
+        for text_element in text_elements:
+            lines = text_element.split('\n')
+            for line in lines:
+                if targetTerm in line:
+                    report += "\nLine with match:\n" + line.strip() +"\n"
+        #Return the report sting so I can use it in the GUI
+        return report
 
 
 def qScrape(startignUrl: str, targetTerm: str):
-    report = ""
     #I need to keep track of all visited URLs
     visitedURL = set()
     #Store the report as a string (only 1 for this scrape method)
-    firstPage = wikiPage(startignUrl, 0)
+    firstPage = wikiPage(startignUrl, 0, [startignUrl])
     #I will store the pages in a queue and add the first page to it
-    pageQueue = []
+    pageQueue = deque()
     pageQueue.append(firstPage)
 
-    #Invariant is that pages on the Queue need to be searched, and I know how many links away they are from the start
+    #Continue until the queue is empty or a match is found
     while(len(pageQueue)> 0):
-        currPage = pageQueue.pop(0)
+        currPage = pageQueue.popleft()
         # Sending an HTTP GET request to the current URL
         response = requests.get(currPage.url)
-
         # Checking that the request was successful (status code 200)
         if response.status_code != 200:
             print('Failed to retrieve the page, Error Code:', response.status_code)
 
-        # Parsing the HTML content of the page w
+        # Parsing the HTML content of the page with beautiful soup
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Here, I am searching for a term within the text of the page
+        # Search for the target term within the text
         if targetTerm in soup.get_text():
-            print("\nURL: ", currPage.url)
-            print("Links Away:", currPage.linksAway)
-            print("Found:", targetTerm +"\n")
-            report = "\nURL: "+ str(currPage.url) + "\nLinks Away: " + str(currPage.linksAway)
-
-            # Find all text elements in the current text that contain the targetTerm
-            text_elements = soup.find_all(text=lambda text: text and targetTerm in text)
-            # Finding the line containing the match for the final report
-            for text_element in text_elements:
-                lines = text_element.split('\n')
-                for line in lines:
-                    if targetTerm in line:
-                        # Print the line containing the match and add it to the report
-                        print("Line with match:")
-                        print(line.strip()) 
-                        print("\n")
-                        report += "\nLine with match:\n" + line.strip() +"\n"
-            #Return the report sting so I can use it in the GUI
-            return report
+            finalReport = generateReport(urlMatch= currPage ,targetTerm= targetTerm, soup= soup)
+            print(finalReport)
+            return finalReport
 
         # If I dont find a match, I'll add all linked articles to the back of the queue
         else:
@@ -69,7 +67,9 @@ def qScrape(startignUrl: str, targetTerm: str):
                         full_link = 'https://en.wikipedia.org' + link
                         if full_link not in visitedURL:
                             #Making my new page object for the linked page
-                            linkedPage = wikiPage(full_link, currPage.linksAway +1)
+                            newPath = currPage.path.copy()
+                            newPath.append(full_link)
+                            linkedPage = wikiPage(full_link, currPage.linksAway +1, newPath)
                             pageQueue.append(linkedPage)
                         else:
                             continue
@@ -80,6 +80,6 @@ def qScrape(startignUrl: str, targetTerm: str):
   
 
 #Test
-    #qScrape('https://en.wikipedia.org/wiki/Michael_Scott_(The_Office)', 'death')
+# qScrape('https://en.wikipedia.org/wiki/Michael_Scott_(The_Office)', 'trailer park')
 
 
